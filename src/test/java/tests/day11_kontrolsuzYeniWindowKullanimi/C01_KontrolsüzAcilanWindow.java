68a5dd6ac6b1a26c565ec9585e5b6120b54798a9
cennetatak <cennetatak1992@gmail.com>
2025-05-20 08:51:10 +0300

    11.gün

---
+++ src/test/java/tests/day11_kontrolsuzYeniWindowKullanimi/C01_KontrolsüzAcilanWindow.java
@@ -0,0 +1,18 @@
+package tests.day11_kontrolsuzYeniWindowKullanimi;
+
+import org.junit.jupiter.api.Test;
+
+public class C01_KontrolsüzAcilanWindow {
+
+
+    @Test
+    public void test01(){
+
+        //1- https://testotomasyonu.com/discount adresine gidin
+
+        //2- Fashion bolumunde ilk urunu tiklayin
+
+        //3- acilan urun sayfasinda urun ismininin
+        //   case sensitive olmadan dress icerdigini test edin
+    }
+}
